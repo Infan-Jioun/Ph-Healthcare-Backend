69d898ab2527172494b48e4ffa910653355afefb
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-02-15 22:35:54 +0600

    added global eror handler

---
+++ src/middleware/globalHandleError.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
+
+export const globalError = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (envVars.NODE_ENV === "development") {
+        console.log("Error from global error Handler", err);
+    }
+    const statusCode: number = 500;
+    const message: string = "Internal server error ";
+    res.status(statusCode).json({
+        success: false,
+        message: message,
+        error: err.message
+    })
+}
