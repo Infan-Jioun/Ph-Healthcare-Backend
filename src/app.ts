02c9e2fbdfc0fa7bc80ab3c7bf5c815f07e68b51
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-02-15 16:14:26 +0600

    added auth pattern

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import { SpecialityRouter } from "./app/module/speciality/speciality.router";
+import { authRouter } from "./app/module/auth/auth.router";
 
 const app: Application = express()
 app.use(express.urlencoded({ extended: true }));
@@ -7,6 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/specialities", SpecialityRouter)
 app.get('/', (req, res) => {
     res.send("Ph Healthcare successfully running")
