74b2a3c055685cc28f63b17b5c4165fd9afc3ada
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-02-16 11:52:15 +0600

    added createDoctor client

---
+++ src/app/module/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../../shared/catchAsync";
+import { userService } from "./user.service";
+import { sendResposne } from "../../../shared/sendResponse";
+import status from "http-status";
+
+const createDoctor = catchAsync(
+    async (req: Request, res: Response) => {
+        const payload = req.body;
+        const result = await userService.createDoctor(payload);
+        sendResposne(res, {
+            httpStatusCode: status.CREATED,
+            success: true,
+            message: "Doctor Registered Successfully",
+            data: result
+        })
+
+    }
+)
+export const userContoller = {
+    createDoctor
+}
