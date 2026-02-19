79000e15d8949fbb255810bc26b34a3b0d97a36a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-02-19 15:04:30 +0600

    added create token

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,3 +1,7 @@
-const createToken = {};
-const verifyToken = {};
-const decodeToken = {}
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+const createToken = (payload: JwtPayload, secret: string, { expiresIn }: SignOptions) => {
+    const token = jwt.sign(payload, secret, { expiresIn })
+    return token;
+};
+const verifyToken = () => { };
+const decodeToken = () => { }
