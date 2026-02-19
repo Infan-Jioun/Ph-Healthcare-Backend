78813686a41a40d04d0e9d70e2a1af2311ecbfc1
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-02-19 15:08:19 +0600

    added verify token

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,7 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 const createToken = (payload: JwtPayload, secret: string, { expiresIn }: SignOptions) => {
     const token = jwt.sign(payload, secret, { expiresIn })
     return token;
 };
-const verifyToken = () => { };
+const verifyToken = (token: string, secret: string) => {
+    try {
+        const deocoded = jwt.verify(token, secret) as JwtPayload;
+        return {
+            success: true,
+            data: deocoded
+        }
+    } catch (error: any) {
+        return {
+            success: false,
+            message: error.message,
+            error
+        }
+    }
+};
 const decodeToken = () => { }
