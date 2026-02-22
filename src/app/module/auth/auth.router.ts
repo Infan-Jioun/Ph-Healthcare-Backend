e61c62a5bd8849d77cab23689f423c1e8d820a0d
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-02-22 15:09:23 +0600

    isInvalidSessionExists checker added

--- src/app/module/auth/auth.router.ts
+++ src/app/module/auth/auth.router.ts
@@ -7,4 +7,5 @@ const router = express.Router();
 router.post("/register", authController.registerPatient)
 router.post("/login", authController.loginPatient)
 router.get("/me", checkAuth(Role.SUPER_ADMIN, Role.ADMIN, Role.DOCTOR, Role.PATIENT), authController.getMe)
+router.post("/refresh-token", authController.getNewToken)
 export const authRouter: Router = router;  
