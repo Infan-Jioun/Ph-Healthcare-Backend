9431fa44ff450c7e29083d5be84d2cdb0d0bc0f1
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-02-25 03:48:37 +0600

    Google Login Happens in Better Auth And Configuring EJS as Template Engine

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,11 @@ import { adminRouter } from "./app/module/admin/admin.router";
 import { superAdminRouter } from "./app/module/superAdmin/superAdmin.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./app/lib/auth";
+import path from "path";
 const app: Application = express()
 app.use(express.urlencoded({ extended: true }));
-
+app.set("view engine", "ejs")
+app.set("views", path.resolve(process.cwd(), `src/app/templates`))
 app.use("/api/auth", toNodeHandler(auth))
 // Middleware to parse JSON bodies
 app.use(express.json());
