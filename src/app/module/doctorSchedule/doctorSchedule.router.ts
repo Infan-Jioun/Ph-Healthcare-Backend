1ef8b7da99c3dbf7020635fce408b5beb7fe1965
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-03-12 02:59:17 +0600

    create createMydoctorSchedule controller and setup service with connect to router

--- src/app/module/doctorSchedule/doctorSchedule.router.ts
+++ src/app/module/doctorSchedule/doctorSchedule.router.ts
@@ -1,6 +1,7 @@
 import expres, { Router } from "express";
 import { checkAuth } from "../../../middleware/checkAuth";
 import { Role } from "../../../generated/prisma/enums";
+import { doctorSchedule } from "./doctorSchedule.controller";
 const router = expres.Router();
-router.post("/create-my-doctor-schedule", checkAuth(Role.DOCTOR))
+router.post("/create-my-doctor-schedule", checkAuth(Role.DOCTOR), doctorSchedule.createMyDoctorSchedule)
 export const doctorScheduleRouter: Router = router;
