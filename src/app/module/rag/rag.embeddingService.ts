d875c34c8e9a6c3b1a292cbd17ff9d503d551532
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-05-31 11:11:39 +0600

    feat: update EmbeddingService to use new default embedding model

--- src/app/module/rag/rag.embeddingService.ts
+++ src/app/module/rag/rag.embeddingService.ts
@@ -1,4 +1,19 @@
+import status from "http-status";
+import { envVars } from "../../../config/env";
+import AppError from "../../errorHelper/appError";
+
 export class EmbeddingService {
-    private: apiKey: string;
-    private: apiUrl: string = "https://openrouter.ai/api/v1"
+    private apiKey: string;
+    private apiUrl: string = "https://openrouter.ai/api/v1";
+    private embeddingModel: string;
+    constructor() {
+        this.apiKey = envVars.OPENROUTER_API_KEY || "";
+        this.embeddingModel = envVars.OPENROUTER_EMBEDDING_MODEL || "nvidia/nemotron-3-super-120b-a12b:free";
+        if (!this.apiKey) {
+            throw new AppError(status.NOT_FOUND, "OPENROUTER_API_KEY is not set");
+        }
+        if (!this.embeddingModel) {
+            throw new AppError(status.NOT_FOUND, "OPENROUTER_EMBEDDING_MODEL is not set");
+        }
+    }
 }
