317688c790adbd575a21c0eb46d58e6e23e9d277
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-06-10 12:21:24 +0600

    connect redis

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app";
+import { redisService } from "./app/lib/redis";
 import { envVars } from "./config/env";
 
-const boostrap = () => {
+const boostrap = async () => {
     try {
+        await redisService.connect();
         app.listen(envVars.PORT, () => {
             console.log(`Server is running on http://localhost:${envVars.PORT}`);
         });
